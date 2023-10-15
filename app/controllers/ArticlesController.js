2d42b8d202d71b0977249273bb507a7ce891ac68
Denis Monteiro <deniswmonteiro@gmail.com>
2023-10-15 02:43:08 -0300

    Salvando um artigo no banco de dados

--- app/controllers/ArticlesController.js
+++ app/controllers/ArticlesController.js
@@ -1,11 +1,31 @@
 const express = require("express");
 const router = express.Router();
+const slugify = require("slugify");
+const Article = require("../models/Article");
+const Category = require("../models/Category");
 
-router.get("/artigos", (req, res) => {
+router.get("/admin/artigos", (req, res) => {
     res.send("Artigos");
 });
 
 /** Create a article */
-router.get("/admin/artigos/criar", (req, res) => res.render("admin/articles/create"));
+router.get("/admin/artigos/criar", (req, res) => {
+    Category.findAll()
+        .then((categories) => res.render("admin/articles/create", { categories }));
+});
+
+/** Store a article */
+router.post("/admin/artigos/salvar", (req, res) => {
+    const title = req.body.title;
+    const content = req.body.content;
+    const category = req.body.category;
+
+    Article.create({
+        title,
+        slug: slugify(title).toLowerCase(),
+        content,
+        categoryId: category
+    }).then(() => res.redirect("/admin/artigos"));
+});
 
 module.exports = router
