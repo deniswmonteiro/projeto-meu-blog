309992b0d7d0b99b86bb0e1f776d8653548c30cb
Denis Monteiro <deniswmonteiro@gmail.com>
2023-10-15 00:52:51 -0300

    Adicionando o TinyMCE

--- app/controllers/ArticlesController.js
+++ app/controllers/ArticlesController.js
@@ -5,4 +5,7 @@ router.get("/artigos", (req, res) => {
     res.send("Artigos");
 });
 
+/** Create a article */
+router.get("/admin/artigos/criar", (req, res) => res.render("admin/articles/create"));
+
 module.exports = router
